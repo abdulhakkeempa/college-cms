bd85a58d753c4b15d69ba957993a6ceb970b8136
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2022-12-14 08:44:35 +0530

    some errors

--- resources/views/admin/courses.blade.php
+++ resources/views/admin/courses.blade.php
@@ -1,10 +1,31 @@
-@extends(layouts.master)
+@extends(layouts/master)
 @section('title')
     Courses - Department of Computer Science
 @endsection
 
 
 @section('content')
+<aside id="sidebar" class="sidebar">
+
+<ul class="sidebar-nav" id="sidebar-nav">
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="{{ url('profile') }}">
+      <i class="bi bi-grid"></i>
+      <span>Profile</span>
+    </a>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link" href="{{ url('users') }}">
+      <i class="bi bi-grid"></i>
+      <span>Users</span>
+    </a>
+  </li>
+
+</ul>
+
+</aside><!-- End Sidebar-->
 
 
 @endsection
