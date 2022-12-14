9087adf112a6b5e1293fa5f38d3b0b67d0c6cf8a
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2022-12-14 08:39:48 +0530

    users completed

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -44,16 +44,14 @@
             <div class="col-12 d-flex">
                 <div class="input-group mb-3">
                     <input type="text" class="form-control" aria-label="Text input with dropdown button">
-                    <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
+                    <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">All</button>
                     <ul class="dropdown-menu dropdown-menu-end">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Separated link</a></li>
+                        <li><a class="dropdown-item" href="#">System Admin</a></li>
+                        <li><a class="dropdown-item" href="#">Teachers</a></li>
+                        <li><a class="dropdown-item" href="#">Office Staff</a></li>
                     </ul>
                 </div>
-                <div>
+                <div class="">
                     <a href="" class="btn btn-primary">Create New User</a>
                 </div>
             </div>
