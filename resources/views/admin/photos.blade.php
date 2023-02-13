58e2a5a074e842ddefc90a756738999d13144487
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-02-13 10:16:31 +0530

    frontend: footer for images upload modal removed

--- resources/views/admin/photos.blade.php
+++ resources/views/admin/photos.blade.php
@@ -134,6 +134,32 @@
                 </div>
             </div>
         </div>
+
+        <div class="modal fade" id="photosModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Add New Album</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form method="POST" action="/photos/albums" id="photo_form">
+                        @csrf
+                        <div class="form-floating mb-3">
+                            <input type="file" class="form-control" id="images" name="images" multiple>
+                            <label for="images">Images</label>
+                        </div>                                                                       
+                        <button type="submit" class="btn btn-primary">Submit</button>                  
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" type="submit">Save changes</button>
+                </div>
+                </div>
+            </div>
+        </div>
+        </div>
       </div>
     </div>
   </section>
@@ -149,7 +175,7 @@
                 <img src="https://www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">{{$album->album_title}}</h5>
-                  <a href="#" class="btn btn-primary">Add Photos</a>
+                  <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#photosModal">Add Photos</a>
                   <a href="#" class="btn btn-success">Edit</a>
                   <a href="#" class="btn btn-danger">Delete</a>
                 </div>
