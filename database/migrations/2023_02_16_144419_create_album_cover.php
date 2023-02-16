166bc7bbbc2e5b2b2684ccd814da4b2e4ac34b23
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-02-16 20:25:49 +0530

    albums cover table created

---
+++ database/migrations/2023_02_16_144419_create_album_cover.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('album_cover', function (Blueprint $table) {
+            $table->unsignedBigInteger('album_id');
+            $table->unsignedBigInteger('photo_id')->nullable();
+
+            //setting the foreign key
+            $table->foreign('album_id')->references('album_id')->on('albums')->onDelete('cascade');
+            $table->foreign('photo_id')->references('photo_id')->on('photos')->onDelete('set null');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('album_cover');
+    }
+};
