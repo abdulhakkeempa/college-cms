e6064d16aa89bd139212166c0be1e414f56ae10a
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-02-17 23:13:31 +0530

    update in modal title

--- resources/views/admin/news.blade.php
+++ resources/views/admin/news.blade.php
@@ -224,7 +224,7 @@
               <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                 <div class="modal-content">
                   <div class="modal-header">
-                      <h1 class="modal-title fs-5" id="exampleModalLabel">Create New User</h1>
+                      <h1 class="modal-title fs-5" id="exampleModalLabel">Create News</h1>
                       <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body">
@@ -259,7 +259,7 @@
               <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                 <div class="modal-content">
                   <div class="modal-header">
-                      <h1 class="modal-title fs-5" id="exampleModalLabel">Create New User</h1>
+                      <h1 class="modal-title fs-5" id="exampleModalLabel">Edit News</h1>
                       <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body">
