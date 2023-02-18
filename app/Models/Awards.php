b4446201b9353cd5cc956385b7c2b76dcb50b6da
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-02-18 15:00:51 +0530

    model done for awards table

--- app/Models/Awards.php
+++ app/Models/Awards.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class Awards extends Model
 {
     use HasFactory;
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'awards';
+
+    //overwriting the default setting to avoid insertion of timestamp to db.
+    public $timestamps = false;
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'award_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'student_name',
+        'course_id',
+        'award_desc',
+        'batch',
+    ];
 }
