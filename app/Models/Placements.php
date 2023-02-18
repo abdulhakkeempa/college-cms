67c64bee4f9bfaa359fbe553f7cc90e55c5faa61
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-02-18 14:30:18 +0530

    model for placements done

--- app/Models/Placements.php
+++ app/Models/Placements.php
@@ -8,4 +8,32 @@ use Illuminate\Database\Eloquent\Model;
 class Placements extends Model
 {
     use HasFactory;
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'placements';
+
+    //overwriting the default setting to avoid insertion of timestamp to db.
+    public $timestamps = false;
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'placement_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'student_name',
+        'course_id',
+        'batch',
+        'company',
+        'job_role',
+    ];
 }
