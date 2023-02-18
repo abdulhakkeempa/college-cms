32a10b15e9208f8a85b540f77266e602bc6f3549
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-02-18 14:25:07 +0530

    migration for placement table done

--- database/migrations/2023_02_18_085106_create_placements_table.php
+++ database/migrations/2023_02_18_085106_create_placements_table.php
@@ -14,8 +14,15 @@ return new class extends Migration
     public function up()
     {
         Schema::create('placements', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id('placement_id');
+            $table->string('student_name');
+            $table->unsignedBigInteger('course_id');
+            $table->string('batch');
+            $table->string('company');
+            $table->string('job_role')->nullable();
+
+            //setting the foreign key, deletes the object when particular course is deleted.
+            $table->foreign('course_id')->references('album_id')->on('courses')->onDelete('cascade');
         });
     }
 
