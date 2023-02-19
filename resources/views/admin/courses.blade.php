ccb7e86bef5b6aad050218bffc6254e63521f656
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-02-19 15:31:05 +0530

    error warning text color changed

--- resources/views/admin/courses.blade.php
+++ resources/views/admin/courses.blade.php
@@ -396,7 +396,7 @@
           <div class="alert alert-danger">
             <ul>
                 @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
+                    <li class="text-black" >{{ $error }}</li>
                 @endforeach
             </ul>
           </div>
