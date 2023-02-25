d378f04293f116b7cde6db7cc30b7c1bef3cdf27
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-02-25 16:54:35 +0530

    removed password from user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,6 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'designation' => 'Assistant Professor',
             'phone_number' => 1000000000,
             'address' => fake()->address(),
